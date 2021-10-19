863a7337cb8dd6aeddc589d9fd6de9e13b303496
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2021-10-19 15:32:21 +0530

    Add functionality (CS1)
    Add functionality to function compute_statistics

--- stats.c
+++ stats.c
@@ -1,10 +1,23 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
+    float sum, maximum, minimum;
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    for(i=0;i<=setlength;i++)
+    {
+        sum += numberset[i];
+        maximum = MAX(maximum,numberset[i]);
+        minimum = MIN(minimum,numberset[i]);
+    }
+    s.average = sum/setlength;
+    s.min = minimum;
+    s.max = maximum;
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
