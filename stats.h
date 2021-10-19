b09e89ac8566da12a241d79dbe3eb1e8cbc1ea53
PavanKumar-Gudiwada <91024463+PavanKumar-Gudiwada@users.noreply.github.com>
2021-10-19 15:46:29 +0530

    create Stats structure
    create Stats structure

--- stats.h
+++ stats.h
@@ -2,6 +2,13 @@
 #define MAX(a, b)  ( ((a) > (b)) ? (a) : (b) )
 #define MIN(a, b)  ( ((a) < (b)) ? (a) : (b) )
 
+typedef struct
+{
+  float average;
+  float min;
+  float max;
+}Stats;
+
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
